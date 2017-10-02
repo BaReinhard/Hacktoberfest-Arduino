03310fb8f048d84162b55db53cd62d2a84fc6e27
Brett Reinhard <brett@reinhards.us>
2017-10-01 19:42:06 -0700

    Removed remnants

--- RGB-Classes/curry.h
+++
@@ -1,7 +0,0 @@
-template <typename Function, typename... Arguments>
-auto curry(Function func, Arguments... args)
-{
-    return [=](auto... rest) {
-        return func(args..., rest...);
-    };
-}
